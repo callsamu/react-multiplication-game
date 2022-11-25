e5c54392477640f75f033040fd46e7dd7fd70eeb
callsamu <samuelbree@gmail.com>
2022-11-24 21:44:44 -0300

    implement panel for answer exibition and question request

---
+++ src/AnswerPanel.tsx
@@ -0,0 +1,30 @@
+import { Button, Container, Heading, Text, VStack } from "@chakra-ui/react";
+import { MouseEventHandler } from "react";
+
+type QuestionRequest = React.DispatchWithoutAction;
+
+function NewQuestionButton({ onClick }: { onClick: MouseEventHandler<HTMLButtonElement> }) {
+  return <Button p="5" onClick={onClick}> Go to a New Problem </Button>;
+}
+
+type AnswerProps = {
+  answer: number
+  userAnswer: number | null
+  questionRequest: QuestionRequest
+}
+function Answer(props: AnswerProps) {
+  const { answer, userAnswer } = props;
+
+  const isCorrect = (answer === userAnswer);
+  return (
+    <Container centerContent>
+      <VStack>
+        <Heading color={ isCorrect ? "green" : "red" } >
+          { isCorrect ? "RIGHT!" : "WRONG!" }
+        </Heading>
+        <Text fontSize="20"> answer was { answer } </Text>
+        <NewQuestionButton onClick={() => props.questionRequest()}/>
+      </VStack>
+    </Container>
+  );
+}
